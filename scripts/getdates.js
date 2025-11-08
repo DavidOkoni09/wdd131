fba36ad4d8c4300e0332f8baeedbce0d634f3cf4
DavidOkoni09 <vee2004.david@gmail.com>
2025-11-08 08:11:19 +0100

    repairs and debugging

--- scripts/getdates.js
+++ scripts/getdates.js
@@ -2,4 +2,4 @@ const currentYear = new Date().getFullYear();
 document.getElementById("currentyear").innerHTML = currentYear;
 
 const lastModified = document.lastModified;
-document.getElementById("lastModified").textContent = `Last Modified: ${lastModified}`;
+document.getElementById("lastModified").innerHTML = `Last Modified: ${lastModified}`;
