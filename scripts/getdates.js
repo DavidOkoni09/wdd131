ad6ccd5fea9cb75e8d10730440cffe7b20c86906
DavidOkoni09 <oagrinya1@byupathway.edu>
2026-01-07 16:12:21 +0100

    responsive edit

--- scripts/getdates.js
+++ scripts/getdates.js
@@ -3,3 +3,12 @@ document.getElementById("currentyear").innerHTML = currentYear;
 
 const lastModified = document.lastModified;
 document.getElementById("lastModified").innerHTML = `Last Modified: ${lastModified}`;
+
+
+const navbutton = document.querySelector("#ham-btn");
+const nav = document.querySelector("#nav-bar");
+
+navbutton.addEventListener("click", () => {
+    navbutton.classList.toggle("show");
+    nav.classList.toggle("show");
+})
